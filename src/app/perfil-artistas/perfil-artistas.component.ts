60c8fabbb740b3fbf050f77d76944ed67a3037e8
Duvancho09 <duvanj603@hotmail.com>
2020-09-17 20:54:32 -0500

    nuevo

--- src/app/perfil-artistas/perfil-artistas.component.ts
+++ src/app/perfil-artistas/perfil-artistas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-perfil-artistas',
   templateUrl: './perfil-artistas.component.html',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilArtistasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private app:AppComponent) { }
 
   ngOnInit(): void {
+    this.app.FalseToolBar();
   }
 
 }
