d98e0d19f1415874cd9380a98a4353fc2adf83b2
Duvancho09 <duvanj603@hotmail.com>
2020-10-03 01:38:12 -0500

    Se actualizo el perfil artista y se adiciono boton para subir imagen

--- src/app/perfil-artistas/perfil-artistas.component.ts
+++ src/app/perfil-artistas/perfil-artistas.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { MatDialog } from '@angular/material/dialog';
 import { NotificacionesComponent } from '../notificaciones/notificaciones.component';
+import { InicioComponent } from '../inicio/inicio.component';
+import { InicioArtistasComponent } from '../inicio-artistas/inicio-artistas.component';
+import { GeneralComponent } from '../general/general.component';
+import { InicioAdminComponent } from '../inicio-admin/inicio-admin.component';
+
 
 @Component({
   selector: 'app-perfil-artistas',
@@ -22,5 +27,18 @@ onUpload(){
   }
   openInfo(){
     this.dialog.open(NotificacionesComponent);
+  }
+  /*openArtistas(){
+    this.dialog.open(ini);
+  }*/
+  url="./assets/blanco.jpg";
+  onselectFile(e){
+    if(e.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+      }
+    }
   }
 }
