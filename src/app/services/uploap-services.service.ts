60b7751e5c4bca24b280f1d96d1ffb998bc3a5cf
Duvancho09 <duvanj603@hotmail.com>
2020-10-24 01:06:07 -0500

    Se agrego lo de la api para salir el error de Cors

--- src/app/services/uploap-services.service.ts
+++ src/app/services/uploap-services.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +13,8 @@ export class UploapServicesService {
 
   pushFileToStorage(file: File): Observable<HttpEvent<{}>> {
     const data: FormData = new FormData();
-
+    
+    console.log(data)
     data.append('file', file);
 
     const newRequest = new HttpRequest('POST', 'http://localhost:8080/uploadFile', data, {
--- src/app/subir-archivo/subir-archivo.component.ts
+++ src/app/subir-archivo/subir-archivo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import { HttpResponse, HttpEventType } from '@angular/common/http';
 import { UploapServicesService} from '../services/uploap-services.service';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-subir-archivo',
@@ -11,45 +11,67 @@ import { UploapServicesService} from '../services/uploap-services.service';
 })
 export class SubirArchivoComponent implements OnInit {
 /*private image:any;*/
+rta:[];
   datosarchivo = new FormGroup({
     titulo:new FormControl('', [Validators.required]),
     DescripcionPost: new FormControl('',[Validators.required]),
     imagePost: new FormControl('',[Validators.required]),
     idPost: new FormControl('',[Validators.required]),
   });
-  hide=true;
+  
   selectedFiles: FileList;
   currentFileUpload: File;
   progress: { percentage: number } = { percentage: 0 };
   selectedFile = null;
   changeImage = false;
-  constructor(private uploadService: UploapServicesService,) { }
+  
+  constructor(private uploadService: UploapServicesService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    //this.http.get('')
+    //.subscribe(data =>{
+
+    //});
+  }
+  onSubmit(){
+    console.log(this.datosarchivo.value);
+    
   }
 
+  downloadFile(){
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '_File_Saved_Path');
+    link.setAttribute('download', 'file_name.pdf');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+  change($event) {
+    this.changeImage = true;
+  }
+
+  changedImage(event) {
+    this.selectedFile = event.target.files[0];
+  }
   /*url="";*/
-  handleImage(e){
-    /*this.image = event.target.file[0];*/
+  /*handleImage(e){
+    this.image = event.target.file[0];
     console.log('Image', e.target.files[0]);
+    this.currentFileUpload = e.target.files.item(0);
     /*if(e.target.files){
       var reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
       reader.onload=(event:any)=>{
         this.url=event.target.result;
       }
-    }*/
-  }
-
-  onSubmit(){
-    console.log(this.datosarchivo.value);
-    
-  }
+    }
+  }*/
   upload() {
-    console.log("duvancho")
+    //console.log("duvancho")
     this.progress.percentage = 0;
 
-    this.currentFileUpload = this.selectedFiles.item(0);
+    this.currentFileUpload = this.selectedFiles.item[0];
     this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
       if (event.type === HttpEventType.UploadProgress) {
         this.progress.percentage = Math.round(100 * event.loaded / event.total);
@@ -61,6 +83,9 @@ export class SubirArchivoComponent implements OnInit {
     this.selectedFiles = undefined;
       }
     );
+  }
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
   }
 
 }
